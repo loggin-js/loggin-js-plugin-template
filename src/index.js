2ccf6c5aedff2e992388b3682ea2e3992b0693ae
nombrekeff <manoloedge96@gmail.com>
2020-08-29 11:51:57 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const loggin = require('loggin-js');
+
+/**
+ * @param {loggin} loggin 
+ */
+function plugin(loggin) {
+    const { Notifier, Logger, Severity } = loggin;
+}
+
+module.exports = plugin;
